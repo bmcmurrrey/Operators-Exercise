5c2d3cb988891db71efd4e8ae66fe1bc60383d74
Blake <blakemcmurrey@gmail.com>
2023-06-06 10:48:23 -0500

    Completed exercises

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,24 @@
 ﻿namespace OperatorExercise
 {
     public class Program
-    {
-        static void Main(string[] args)
+    { static void Main(string[] args)
+       {
+        int a = 17;
+        int b = 4;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        Console.WriteLine($"{a} / {b} is equal to {quotient} and the remainder is {remainder}");
+        Console.WriteLine("Enter the radius of your circle");
+        var userInput = Console.ReadLine();
+        var radius = double.Parse(userInput);
+        Console.WriteLine(AreaOfCircle(radius));
+        }
+      public static double AreaOfCircle(double radius)
         {
-            Console.WriteLine("Hello, World!");
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            return area;
         }
     }
 }
